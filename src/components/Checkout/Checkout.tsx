ed970b6696b835f1b2093c1413b990eb939620c9
Balaban <neddmad1@gmail.com>
2024-07-10 19:49:42 +0300

    .

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -13,28 +13,30 @@ function Checkout({ order }: checkoutInterface) {
 
   return (
     <div className={styles.checkout}>
-      <h1>Ваше замовлення :</h1>
-      <ul className={styles.checkoutList}>
-        {order.map((elem) => (
-          <li key={elem.id}>
-            {elem.topping && elem.topping.length !== 0
-              ? elem.name + " with " + elem.topping
-              : elem.name}
-          </li>
-        ))}
-      </ul>
-      <h2>
-        На суму:{" "}
-        <a className={styles.checkoutNumber}>
-          {order.reduce(
-            (init, elem) =>
-              elem.quantity && typeof elem.quantity === "number"
-                ? (init += elem.price * elem.quantity)
-                : init + elem.price,
-            0
-          ) + " $"}
-        </a>
-      </h2>
+      <div className={styles.center}>
+        <h1>Ваше замовлення :</h1>
+        <ul className={styles.checkoutList}>
+          {order.map((elem) => (
+            <li key={elem.id}>
+              {elem.topping && elem.topping.length !== 0
+                ? elem.name + " with " + elem.topping + " - " + elem.quantity
+                : elem.name + " - " + elem.quantity}
+            </li>
+          ))}
+        </ul>
+        <h2>
+          На суму:{" "}
+          <a className={styles.checkoutNumber}>
+            {order.reduce(
+              (init, elem) =>
+                elem.quantity && typeof elem.quantity === "number"
+                  ? (init += elem.price * elem.quantity)
+                  : init + elem.price,
+              0
+            ) + " $"}
+          </a>
+        </h2>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,48 +15,56 @@ function Header() {
     functionSetLocation,
     openBurgerMenu,
     togglePop,
+    isLoaded,
   } = useContext(MyContext);
   //localStorage
   let localStorageValues = Object.values(localStorage);
-
-  return (
-    <div className={styles.header}>
-      <span className={styles.menuIcon}>
-        <MenuIcon onClick={openBurgerMenu} />
-      </span>
-      <div className={styles.reactIcon}>
-        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-          REACT PIZZA
-        </Link>
-        <img style={{ width: "30px", height: "30px" }} src={pizzaGif} alt="" />
-      </div>
-      <form>
-        <select
-          className={styles.adressForm}
-          value={locationRestaurant}
-          onChange={functionSetLocation}
-        >
-          <option style={{ color: "black" }}>
-            {getRestaurant.map((e) => (e.id === 1 ? e.address1 : null))}
-          </option>
-          <option style={{ color: "black" }}>
-            {getRestaurant.map((e) => (e.id === 2 ? e.address1 : null))}
-          </option>
-        </select>
-      </form>
-      <ThemeSwitcher />
-      <IsUserLogged togglePop={togglePop} />
-      <div className={styles.displayFlex}>
-        <ShoppingCartIcon
-          className={styles.shoppingCart}
-          onClick={openNavMenu}
-        />
-        <span className={styles.localStorage}>
-          {!localStorageValues.length ? null : localStorageValues.length}
+  if (isLoaded) {
+    return (
+      <div className={styles.header}>
+        <span className={styles.menuIcon}>
+          <MenuIcon onClick={openBurgerMenu} />
         </span>
+        <div className={styles.reactIcon}>
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            REACT PIZZA
+          </Link>
+          <img
+            style={{ width: "30px", height: "30px" }}
+            src={pizzaGif}
+            alt=""
+          />
+        </div>
+        <form>
+          <select
+            className={styles.adressForm}
+            value={locationRestaurant}
+            onChange={functionSetLocation}
+          >
+            <option style={{ color: "black" }}>
+              {getRestaurant.map((e) => (e.id === 1 ? e.address1 : null))}
+            </option>
+            <option style={{ color: "black" }}>
+              {getRestaurant.map((e) => (e.id === 2 ? e.address1 : null))}
+            </option>
+          </select>
+        </form>
+        <ThemeSwitcher />
+        <IsUserLogged togglePop={togglePop} />
+        <div className={styles.displayFlex}>
+          <ShoppingCartIcon
+            className={styles.shoppingCart}
+            onClick={openNavMenu}
+          />
+          <span className={styles.localStorage}>
+            {!localStorageValues.length ? null : localStorageValues.length}
+          </span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 export default Header;
